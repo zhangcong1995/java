564a774e220c5a17d3e9d843ca1bd20badbadff8
zhangcong1995 <584719545@qq.com>
2019-05-27 14:46:20 +0800

    HelloWord提交

---
+++ HelloWord/src/HelloWord.java
@@ -0,0 +1,20 @@
+/**@文件名: HelloWord.java
+ * @类功能说明: 
+ * @作者: zhangcong
+ * @Email: 584719545@qq.com
+ * @日期: 2019年5月27日下午2:41:06
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: zhangcong</li> 
+ * 	 <li>日期: 2019年5月27日下午2:41:06</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+
+	}
+
+}
